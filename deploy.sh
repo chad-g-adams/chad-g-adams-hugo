181b5c9c61bad2769c112d1a06879fd19c1311b8
chad-g-adams <chad.adams@protonmail.com>
2016-06-24 22:20:59 -0400

    adding cname file

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ rm -r public/*
 # generate new website data
 hugo
 
+# generate CNAME file for custom domain name
+echo 'www.cadams.ca' > public/CNAME
+
 # commit and push the new content
 cd public
 git add -A
